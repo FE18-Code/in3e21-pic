03945f76e3e8282ab0829c7667096adde3f2c354
ams <ams.gh@hmamail.com>
2016-05-11 16:22:11 +0200

    usart print text
    used sci fcts to configure & use (tx) usart
    PutByte() working !

--- led1.c
+++ led1.c
@@ -1,46 +1,31 @@
 #define Attente ;;
 #include <htc.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <conio.h>
+#include "sci.h"
 
 /* bits de configuration */
 __CONFIG(FOSC_HS & WDTE_OFF & PWRTE_OFF & BOREN_OFF & LVP_OFF & CPD_OFF & WRT_OFF & CP_OFF);
 
-unsigned char Compteur;
+unsigned char i;
 void interrupt Timer_Overflow();
 
+const char *text="1234567890";
+
 void main(){
-    Compteur=0;
-    TRISB=0xFE;/* sens du port */
-    
-    /* (xxxx xxxx)&(1111 1101) = (xxxx xx0x) */
-    TRISB&=(0xFD);
-    
-    RB0=0;/* bit 0 à 0 */
-    RB1=0;
-    OPTION_REG=0x7;
-    TMR0=0;
-    
-    /* interrupts */
-    INTCON=0X20;
-    GIE=1;
-    
-    for(Attente){
-      /* dodo */  
-    }
+
+    if(sci_Init(9600,SCI_EIGHT)==true){
+        /* pb */
+    }else{  
+        for(i=0;i<strlen(text);i++){
+            sci_PutByte(text[i]);
+        }
+
+        for(Attente){
+          /* dodo */  
+        }
+    }/* sci_init() */
     
 }/* main */
-
-void interrupt Timer_Overflow(){
-    if(T0IF){/* test flag timer 0 */
-        T0IF=0;/* reset du flag */
-        Compteur++;
-        if(Compteur==2){
-            Compteur=0;
-            RB1=RB0;
-            RB0=~RB0;/* complémenter RB0 */
-            
-            
-            
-            TMR0=0;
-        }
-    }
-}
